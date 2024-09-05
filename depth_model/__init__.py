eed0196eaf66a8b95880d89a7cf6fc8c3b62f92f
Francis Xiatian Zhang <156787438+FrancisXZhang@users.noreply.github.com>
2024-09-05 10:23:53 +0100

    Add files via upload

---
+++ depth_model/__init__.py
@@ -0,0 +1,8 @@
+from .resnet_encoder import ResnetEncoder
+from .depth_decoder import DepthDecoder
+from .depth_inpainter import DepthInpainter
+from .pose_decoder import PoseDecoder
+from .pose_cnn import PoseCNN
+from .appearance_flow_decoder import TransformDecoder
+from .optical_flow_decoder import PositionDecoder
+
